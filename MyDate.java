2c386e1ce50b542fa21a04030864b832ca8a1219
TaufiqNurrohman_2305551 <nurohmantaufik47@gmail.com>
2025-04-29 21:21:21 +0700

    Add files via upload

---
+++ MyDate.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.Soal2;
+
+/**
+ *
+ * @author Taufiq Nurrohan
+ */
+import java.util.GregorianCalendar;
+
+public class MyDate {
+    private int year;
+    private int month;
+    private int day;
+
+    public MyDate() {
+        GregorianCalendar calendar = new GregorianCalendar();
+        this.year = calendar.get(GregorianCalendar.YEAR);
+        this.month = calendar.get(GregorianCalendar.MONTH) + 1;
+        this.day = calendar.get(GregorianCalendar.DAY_OF_MONTH);
+    }
+
+    public MyDate(int year, int month, int day) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+
+    public String toString() {
+        return day + "/" + month + "/" + year;
+    }
+}
+
